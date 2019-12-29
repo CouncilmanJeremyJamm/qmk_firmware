8fabbfc25a7c96ba396bcc1a3bfd32b0d502ea0f
CouncilmanJeremyJamm <47271709+CouncilmanJeremyJamm@users.noreply.github.com>
2019-12-29 13:49:49 +0000

    Changed layout, added names to layers.

--- keyboards/ut472/keymaps/custom/keymap.c
+++ keyboards/ut472/keymaps/custom/keymap.c
@@ -19,6 +19,14 @@
 #define LT3_TAB LT(3, KC_TAB)
 #define BASE_COLOUR HSV_CORAL
 
+/* Names layers in sequential order */
+enum layers {
+    _BASE,
+    _NUMS,
+    _SYMB,
+    _MED,
+};
+
 /* Sets default underglow colour and mode */
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
@@ -29,16 +37,16 @@ void keyboard_post_init_user(void) {
 /* Assigns a unique underglow colour to each layer */
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-    case 3:
+    case _MED:
         rgblight_sethsv_noeeprom(HSV_YELLOW);
         break;
-    case 2:
+    case _SYMB:
         rgblight_sethsv_noeeprom(HSV_MAGENTA);
         break;
-    case 1:
+    case _NUMS:
         rgblight_sethsv_noeeprom(HSV_TEAL);
         break;
-    default:
+    default: //_BASE
         rgblight_sethsv_noeeprom(BASE_COLOUR);
         break;
     }
@@ -46,10 +54,10 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 /* Tapdance */
-enum {
-	LBRACS,
-	RBRACS,
-	TDRESET
+enum tapdances {
+    LBRACS,
+    RBRACS,
+    TDRESET
 };
 
 void lbracs_tapdance(qk_tap_dance_state_t *state, void *user_data) {
@@ -82,93 +90,93 @@ void rbracs_tapdance(qk_tap_dance_state_t *state, void *user_data) {
 
 /* Reset button only activates on a triple tap */
 void tdreset_tapdance(qk_tap_dance_state_t *state, void *user_data) {
-	if (state->count == 3) {
-		rgblight_sethsv_noeeprom (HSV_RED); // Red colour indicates bootloader mode
-		reset_keyboard();
-	}
+    if (state->count == 3) {
+		    rgblight_sethsv_noeeprom (HSV_RED); // Red colour indicates bootloader mode
+		    reset_keyboard();
+	  }
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-	[LBRACS] = ACTION_TAP_DANCE_FN (lbracs_tapdance),
-	[RBRACS] = ACTION_TAP_DANCE_FN (rbracs_tapdance),
-	[TDRESET] = ACTION_TAP_DANCE_FN (tdreset_tapdance)
+    [LBRACS] = ACTION_TAP_DANCE_FN (lbracs_tapdance),
+    [RBRACS] = ACTION_TAP_DANCE_FN (rbracs_tapdance),
+    [TDRESET] = ACTION_TAP_DANCE_FN (tdreset_tapdance)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Base Layer
-   * ,-------------------------------------------------------------------------.
-   * | Esc |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |Bspace |
-   * |-------------------------------------------------------------------------+
-   * |Tab/L3|  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  '  |   ;  |
-   * |-------------------------------------------------------------------------+
-   * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Sh/En|
-   * |-------------------------------------------------------------------------+
-   * | Ctrl| Gui | Alt | App |  L1 |   Space   |  L2  | Left| Down|  Up  |Right|
-   * `-------------------------------------------------------------------------'
-   */
+/* Base Layer
+ * ,-------------------------------------------------------------------------.
+ * | Esc |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |Bspace |
+ * |-------------------------------------------------------------------------+
+ * |Tab/L3|  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  '  |   ;  |
+ * |-------------------------------------------------------------------------+
+ * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Sh/En|
+ * |-------------------------------------------------------------------------+
+ * | Ctrl| Gui | Alt | App |  L1 |   Space   |  L2  | Left| Down|  Up  |Right|
+ * `-------------------------------------------------------------------------'
+ */
 
-LAYOUT(
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_MEDIA_PLAY_PAUSE,MO(1), KC_SPC,  MO(2),  KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT
-),
+    [_BASE] = LAYOUT(
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+      LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_MPLY, MO(1),       KC_SPC,       MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
 
-  /* FN Layer 1
-   * ,-------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Delete|
-   * |-------------------------------------------------------------------------+
-   * |      |     |     |     |     |     |     |  -  |  =  | ([{ | )]} |  \   |
-   * |-------------------------------------------------------------------------+
-   * |       | F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |     |
-   * |-------------------------------------------------------------------------+
-   * |     |     |     |Capsl|      |          |       |     |     |     |     |
-   * `-------------------------------------------------------------------------'
-   */
+/* FN Layer 1, numbers and useful symbols
+ * ,-------------------------------------------------------------------------.
+ * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Delete|
+ * |-------------------------------------------------------------------------+
+ * |      |     |     |     |     |     |     |  -  |  _  |  =  |  +  |  \   |
+ * |-------------------------------------------------------------------------+
+ * |       |     |     |     |     |     |     |     |     |     |     |     |
+ * |-------------------------------------------------------------------------+
+ * |     |     |     |     |      |          |       |     |     |     |     |
+ * `-------------------------------------------------------------------------'
+ */
 
-LAYOUT( /* Left */
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
-  _______, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, KC_MINS, KC_EQL,  TD(LBRACS), TD(RBRACS), KC_BSLS,
-  _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______,
-  _______, _______, _______, KC_CAPS, _______,     _______,      _______, _______, _______, _______, _______
-),
+    [_NUMS] = LAYOUT( /* Left */
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
+      _______, _______, _______, _______, _______, _______, _______, KC_MINS, S(KC_MINS),KC_EQL,S(KC_EQL),KC_BSLS,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+    ),
 
-  /* FN Layer 2
-   * ,-------------------------------------------------------------------------.
-   * |   ~  |  !  |  "  |  #  |  $  |  %  |  ^  |  &  |  *  |  _  |  +  |Delete|
-   * |-------------------------------------------------------------------------+
-   * |      | F1  | F2  | F3  |  F4 |  F5 |  F6 |  F7 | F8  | F9  |  F10 |  |  |
-   * |-------------------------------------------------------------------------+
-   * |       | F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |     |
-   * |-------------------------------------------------------------------------+
-   * |     |     |     |Capsl|      |          |       |     |     |     |     |
-   * `-------------------------------------------------------------------------'
-   */
+/* FN Layer 2, symbols and F-keys
+ * ,-------------------------------------------------------------------------.
+ * |   ~  |  !  |  "  |  #  |  $  |  %  |  ^  |  &  |  *  | ([{ | )]} |Delete|
+ * |-------------------------------------------------------------------------+
+ * |      | F1  | F2  | F3  |  F4 |  F5 |  F6 |  F7 | F8  | F9  |  F10 |  |  |
+ * |-------------------------------------------------------------------------+
+ * |       | F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |     |
+ * |-------------------------------------------------------------------------+
+ * |     |     |     |Capsl|      |          |       |     |     |     |     |
+ * `-------------------------------------------------------------------------'
+ */
 
-LAYOUT( /* Right */
-  KC_TILDE,  KC_EXCLAIM, KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_MINS, KC_EQL, KC_DELETE,
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_PIPE,
-  _______, KC_F11,   KC_F12,   KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,  _______,
-  _______, _______, _______, KC_CAPS, _______,     _______,      _______, _______, _______, _______, _______
-),
+    [_SYMB] = LAYOUT( /* Right */
+      KC_TILDE,  KC_EXCLAIM, KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, TD(LBRACS), TD(RBRACS), KC_DELETE,
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_PIPE,
+      _______, KC_F11,   KC_F12,   KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,  _______,
+      _______, _______, _______, KC_CAPS, _______,     _______,      _______, _______, _______, _______, _______
+    ),
 
-  /* FN Layer 3
-   * ,-------------------------------------------------------------------------.
-   * | Esc | Calc|Webhm| Mail|Files|     |     |     |     |     |PrtSc|  Home |
-   * |-------------------------------------------------------------------------+
-   * |      |     |     |     |     |     |     |     |     |     |     | PgUp |
-   * |-------------------------------------------------------------------------+
-   * |       |     |     |     |     |     |     |     |     |     |     | PgDn|
-   * |-------------------------------------------------------------------------+
-   * |     |     |     |     |      |          |       |MousL|MousU|MousD|MousR|
-   * `-------------------------------------------------------------------------'
-   */
+/* FN Layer 3, media and navigation
+ * ,-------------------------------------------------------------------------.
+ * | Esc |     |     |     |     |     |     |     | Prev| Play| Next |      |
+ * |-------------------------------------------------------------------------+
+ * |      |     |     |     |     |     |     |     |     |     |     |      |
+ * |-------------------------------------------------------------------------+
+ * |       |     |     |     |     |     |     |     |     |     |     |     |
+ * |-------------------------------------------------------------------------+
+ * |     |     |     |     |      |          |       | Home| PgDn| PgUp| End |
+ * `-------------------------------------------------------------------------'
+ */
 
-LAYOUT( /* Tab */
-  KC_ESC,  KC_CALC, KC_WHOM, KC_MAIL, KC_MYCM, _______, _______, _______, _______, _______, KC_PSCR, KC_HOME,
-  _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,
-  _______, _______, _______, _______, _______,TD(TDRESET), _______, _______, _______, _______, _______, KC_PGDN,
-  _______, _______, _______, _______, _______,     _______,      _______, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R
-),
+    [_MED] = LAYOUT( /* Tab */
+      KC_ESC,  _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,
+      _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,TD(TDRESET), _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    ),
 };
