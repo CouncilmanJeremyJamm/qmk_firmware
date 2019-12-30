8daea9846556d4c4feb3753e3ac3c1b2cf588b3d
CouncilmanJeremyJamm <47271709+CouncilmanJeremyJamm@users.noreply.github.com>
2019-12-30 18:54:03 +0000

    Moved media keys

--- keyboards/ut472/keymaps/custom/keymap.c
+++ keyboards/ut472/keymaps/custom/keymap.c
@@ -180,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MED] = LAYOUT( /* Tab */
       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-      _______, RGB_TOG, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,
+      _______, RGB_TOG, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
       _______, _______, _______, _______, _______, TD_RSET, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
